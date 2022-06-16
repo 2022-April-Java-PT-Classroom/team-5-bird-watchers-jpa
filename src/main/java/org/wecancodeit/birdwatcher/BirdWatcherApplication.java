aa462a5603a83fe86598d606c3bf4fe2ed0a9189
jjmirwaldjr <jjmirwaldjr@gmail.com>
2022-06-16 18:43:34 -0400

    renamed the applications

--- src/main/java/org/wecancodeit/birdwatcher/BirdWatcherApplication.java
+++ src/main/java/org/wecancodeit/birdwatcher/BirdWatcherApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 
 
-public class DemoApplication {
+public class BirdWatcherApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BirdWatcherApplication.class, args);
 
 	}
 

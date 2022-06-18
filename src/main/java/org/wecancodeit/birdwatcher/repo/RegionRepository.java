46d7f128e7a049fb062475d83bfe3d4b490d3af5
pennyschuenke <pennyschuenke@gmail.com>
2022-06-18 18:56:22 -0400

    templates and edits

--- src/main/java/org/wecancodeit/birdwatcher/repo/RegionRepository.java
+++ src/main/java/org/wecancodeit/birdwatcher/repo/RegionRepository.java
@@ -1,7 +1,6 @@
 package org.wecancodeit.birdwatcher.repo;
 
 import org.springframework.data.repository.CrudRepository;
-
 import javax.swing.plaf.synth.Region;
 
 public interface RegionRepository extends CrudRepository<Region,Long> {

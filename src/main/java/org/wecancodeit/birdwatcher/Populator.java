929255417fe66c802ee1ea8f5ddd51b52f1a3efd
grullonvictor06 <grullonvictor06@gmail.com>
2022-06-19 19:36:58 -0400

    Created the populator and 2 skeleton templates for country and habitat

---
+++ src/main/java/org/wecancodeit/birdwatcher/Populator.java
@@ -0,0 +1,30 @@
+package org.wecancodeit.birdwatcher;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.swing.plaf.synth.Region;
+
+@Component
+public class Populator implements CommandLineRunner {
+
+    @Resource
+    private RegionRepository regionRepo;
+
+    @Override
+    public void run(String...args) throws Exception {
+
+        regionModel theAndes = new regionModel("The Andes");
+        regionRepo.save(theAndes);
+
+        regionModel theNorth = new regionModel("The North");
+        regionRepo.save(theAndes);
+
+        regionModel thePampas = new regionModel("The Pampas");
+        regionRepo.save(thePampas);
+
+        regionModel patagonia = new regionModel("Patagonia");
+        regionRepo.save(patagonia);
+    }
+}

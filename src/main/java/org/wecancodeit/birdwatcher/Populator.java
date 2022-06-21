20faddcaf84ad462bba2739da55d93949e399a77
jjmirwaldjr <jjmirwaldjr@gmail.com>
2022-06-21 13:45:47 -0400

    had to modify the habitatModel to fix some errors, I added the layout for the grid on for regions, just need to ask a team member a couple of questions.

--- src/main/java/org/wecancodeit/birdwatcher/Populator.java
+++ src/main/java/org/wecancodeit/birdwatcher/Populator.java
@@ -2,6 +2,8 @@ package org.wecancodeit.birdwatcher;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.wecancodeit.birdwatcher.model.regionModel;
+import org.wecancodeit.birdwatcher.repo.RegionRepository;
 
 import javax.annotation.Resource;
 import javax.swing.plaf.synth.Region;
@@ -14,17 +16,17 @@ public class Populator implements CommandLineRunner {
 
     @Override
     public void run(String...args) throws Exception {
-
-        regionModel theAndes = new regionModel("The Andes");
-        regionRepo.save(theAndes);
-
-        regionModel theNorth = new regionModel("The North");
-        regionRepo.save(theAndes);
-
-        regionModel thePampas = new regionModel("The Pampas");
-        regionRepo.save(thePampas);
-
-        regionModel patagonia = new regionModel("Patagonia");
-        regionRepo.save(patagonia);
+//
+//        regionModel theAndes = new regionModel("The Andes");
+//        regionRepo.save(theAndes);
+//
+//        regionModel theNorth = new regionModel("The North");
+//        regionRepo.save(theAndes);
+//
+//        regionModel thePampas = new regionModel("The Pampas");
+//        regionRepo.save(thePampas);
+//
+//        regionModel patagonia = new regionModel("Patagonia");
+//        regionRepo.save(patagonia);
     }
 }

4c7e500dd96e753e7bdcd0e6e408c4d684d3be6b
grullonvictor06 <grullonvictor06@gmail.com>
2022-06-21 20:49:24 -0400

    Populator full

--- src/main/java/org/wecancodeit/birdwatcher/Populator.java
+++ src/main/java/org/wecancodeit/birdwatcher/Populator.java
@@ -2,6 +2,8 @@ package org.wecancodeit.birdwatcher;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.wecancodeit.birdwatcher.model.regionModel;
+import org.wecancodeit.birdwatcher.repo.RegionRepository;
 
 import javax.annotation.Resource;
 import javax.swing.plaf.synth.Region;
@@ -15,16 +17,18 @@ public class Populator implements CommandLineRunner {
     @Override
     public void run(String...args) throws Exception {
 
-        regionModel theAndes = new regionModel("The Andes");
-        regionRepo.save(theAndes);
-
-        regionModel theNorth = new regionModel("The North");
-        regionRepo.save(theAndes);
-
-        regionModel thePampas = new regionModel("The Pampas");
-        regionRepo.save(thePampas);
-
-        regionModel patagonia = new regionModel("Patagonia");
+        regionModel patagonia = new regionModel("Patagonia","Argentina", "About 2 Million","cold and dry", "Image");
         regionRepo.save(patagonia);
+
+        regionModel aysen = new regionModel("Aysen", "Chile", "102,317", "Cool Oceanic", "Image");
+        regionRepo.save(aysen);
+
+        regionModel losLagos = new regionModel("Los Lagos","Chile", "823,204", "Tropical Savanna", "Image");
+        regionRepo.save(losLagos);
+
+        regionModel magallanes = new regionModel("Magallanes", "Chile", "165,5932", "Tundra", "Image");
+        regionRepo.save(magallanes);
+
+
     }
 }
